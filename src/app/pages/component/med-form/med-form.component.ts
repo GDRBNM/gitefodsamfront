c3abf2d7505b9ccfe99e711f2d879486a152d302
GDRBNM <GDRBNM@gmail.com>
2019-05-17 21:03:55 +0200

    ajout pdf

--- src/app/pages/component/med-form/med-form.component.ts
+++ src/app/pages/component/med-form/med-form.component.ts
@@ -3,6 +3,7 @@ import {Subscription} from "rxjs/index";
 import {ToastrService} from "ngx-toastr";
 import {GestionHuissierService} from "../../../../services/gestion-huissier.service";
 import {NotificationDeMEDService} from "../../../../services/notificationDeMEDService";
+import {HttpEventType, HttpResponse} from "@angular/common/http";
 
 declare var $: any;
 
@@ -19,7 +20,8 @@ export class MEDFORMComponent implements OnInit {
   @Input()
   currentClient;
   private selectedFiles;
-
+  private currentFileUpload;
+  progress;
   constructor(private  huissierService :GestionHuissierService,private toastr:ToastrService,private notifMEDService:NotificationDeMEDService) { }
 
   ngOnInit() {
@@ -29,37 +31,89 @@ export class MEDFORMComponent implements OnInit {
   }
 
   onAjouterNotif(MED) {
-console.log(MED);
-let med:any={};
-med={
-  "dateNotification": MED.dateNotification,
-  "soldeNotification":MED.soldeNotification,
-  "statusNotification": 0,
-  "soldeInitiale": MED.soldeInitiale,
-  "dateOctroie": MED.dateOctroie,
-  "numeroCompte": MED.numeroCompte
-}
-console.log(this.currentClient);
+    console.log(MED);
+    let med:any={};
+    med={
+      "dateNotification": MED.dateNotification,
+      "soldeNotification":MED.soldeNotification,
+      "statusNotification": 0,
+      "soldeInitiale": MED.soldeInitiale,
+      "dateOctroie": MED.dateOctroie,
+      "numeroCompte": MED.numeroCompte
+    }
+    console.log(this.currentClient);
 
-MED.huissier= parseInt(MED.huissier);
-let fichier:File=MED.file;
-this.notifMEDService.AjouterNotif(this.currentClient,MED.huissier,med,fichier).
-subscribe(resp=>{
-  console.log(resp);
-    this.toastr.info('Le PV a éte ajouter Avec succes!', "Ajouter un PV!");
-    $('#responsive-modal').modal('toggle');
+    MED.huissier= parseInt(MED.huissier);
 
-},error2 => {
-  console.log(error2);
+    this.notifMEDService.AjouterNotif(this.currentClient,MED.huissier,med).
+    subscribe(resp=>{
+        console.log(resp);
+        let reponse=resp;
+        this.toastr.info('Le PV a éte ajouter Avec succes!', "Ajouter un PV!");
+        $('#responsive-modal').modal('toggle');
+
+        /*
+        *
+        * enregistrement du fichier pdf
+        *
+        */
+
+
+        this.progress=0;
+        //recupere un fichier
+        this.currentFileUpload=this.selectedFiles.item(0);
+
+        //appel au service
+        //on fait un subscribe
+        this.notifMEDService.uploadDonneesPDF(this.currentFileUpload,resp)
+          .subscribe(event=>{
+//      console.log(resp)
+            //  this.toastr.info('Le pdf du PV a éte ajouter Avec succes!', "Ajouter un PV PDF");
+//information de la progression du fichier
+            if (event.type===HttpEventType.UploadProgress){
+              //envent.loaded ce le nombre d'octet uploader et le total l'octet total de la photo
+              //this.progress=Math.round(100*event.loaded/event.total);
+              //   console.log(this.progress)
+            }else if(event instanceof HttpResponse){
+//alert("Fin de telechargement");
+
+              this.progress="";
+            }
+          },error2 => {
+            console.log(error2);
+          })
+
+
+
+
+        /*
+        *
+        *
+        * fin enregistrement du document
+        *
+        *
+        * */
+
+
+
+
+
+
+
+
+
+
+      },error2 => {
+        console.log(error2);
 //  this.toastr.error(error2.error.apierror.message +" "+ error2.error.apierror.debugMessage , 'Erreur');
-  }
-)
+      }
+    )
   }
 
   private listHuissier() {
     this.huissierSubscription= this.huissierService.huissierSubject.subscribe((data)=>{
-      this.Huissier=data;
-    }
+        this.Huissier=data;
+      }
     )
     this.huissierService.getHuissier();
     this.huissierService.emitHiussierSubject();
--- src/services/notificationDeMEDService.ts
+++ src/services/notificationDeMEDService.ts
@@ -2,7 +2,8 @@
 
 import {Injectable} from '@angular/core';
 import {authentificationService} from "./authentification.service";
-import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {HttpClient, HttpEvent, HttpHeaders, HttpRequest} from "@angular/common/http";
+import {Observable} from "rxjs/index";
 
 @Injectable()
 export class NotificationDeMEDService {
@@ -11,10 +12,25 @@ export class NotificationDeMEDService {
   constructor(private authService: authentificationService, private Http: HttpClient) {
   }
 
-  AjouterNotif(client, huissier, MED, file:File) {
+  AjouterNotif(client, huissier, MED) {
 
-    return this.Http.post(this.authService.host + "/SaveMED?idClient=" + client.id + "&idHuissier=" + huissier + "&type=" + client.type + "&file=" + file, MED, {headers: new HttpHeaders({'authorization': this.authService.jwtToken})})
-
+    return this.Http.post(this.authService.host + "/SaveMED?idClient=" + client.id + "&idHuissier=" + huissier + "&type=" + client.type, MED, {headers: new HttpHeaders({'authorization': this.authService.jwtToken})})
 
   }
+
+  uploadDonneesPDF(file: File, resp):Observable<HttpEvent<{}>> {
+    let formadata:FormData =new FormData();
+    //envoyer au serveur du fichier
+    //le terme 'file' est le mm pour l'url du backend
+    formadata.append('file',file);
+    //envoie de formedata a notre url '/uploadPhoto'
+    const  req=new HttpRequest('POST',this.authService.host+'/uploadPdf/'+resp.id,formadata,{
+      //chargement de la progression
+      reportProgress:true,
+      //type de reponse http sous forme de text
+      responseType:'text'
+    });
+    return this.Http.request(req);
+  }
+
 }
